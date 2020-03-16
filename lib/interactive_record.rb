84d4864fa377ac68b6769d7eeebc993be9d19568
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-16 00:50:46 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,7 +49,14 @@ class InteractiveRecord
   end
   
   def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-  DB[:conn].execute(sql, name)
-end
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
+  
+  def self.find_by
+    # sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    # DB[:conn].execute(sql, name)
+  end
+
+
 end
