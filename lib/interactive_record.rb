b1cc3e8239491b38afb1647fde82363f67a2f155
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-16 01:04:51 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
   end
   
   def self.find_by(attribute)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.join} = #{attribute.values} LIMIT 1"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.join} = #{attribute.values.first}"
     DB[:conn].execute(sql)
   end
 
